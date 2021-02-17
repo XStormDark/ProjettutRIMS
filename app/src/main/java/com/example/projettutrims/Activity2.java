e01a9cb2f84926e3c4c891a3ceda7e162e02df38
XStormDark <52172126+XStormDark@users.noreply.github.com>
2021-02-17 18:31:31 +0100

    ajout site

--- app/src/main/java/com/example/projettutrims/Activity2.java
+++ app/src/main/java/com/example/projettutrims/Activity2.java
@@ -4,12 +4,18 @@ package com.example.projettutrims;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Activity2 extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://90.110.215.177/website");
     }
 }
